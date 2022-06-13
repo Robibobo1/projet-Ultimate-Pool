5ebba9246fe70dc58935419a3f3b42dba0b39353
Robin <robincarrupt@gmail.com>
2022-06-14 00:08:16 +0200

    Choix du trou fin de partie

--- src/PoolBall.java
+++ src/PoolBall.java
@@ -5,11 +5,14 @@ import ch.hevs.gdx2d.components.physics.primitives.PhysicsCircle;
 public class PoolBall extends PhysicsCircle {
 	
 	boolean isInHole = false;
+	int holeNbr = 0;
 
 	public PoolBall(String name, Vector2 position, float radius, float density, float restitution, float friction) {
 		super(name, position, radius, density, restitution, friction);
 		// TODO Auto-generated constructor stub
 	}
+	
+	
 
 
 }
